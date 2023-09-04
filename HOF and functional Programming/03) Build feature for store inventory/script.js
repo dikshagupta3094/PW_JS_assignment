741bdd796b1ed3728f3ec8aa94e200d77de627a2
Diksha Gupta <126059790+dikshagupta3094@users.noreply.github.com>
2023-09-04 21:21:52 +0530

    Add files via upload

---
+++ HOF and functional Programming/03) Build feature for store inventory/script.js
@@ -0,0 +1,15 @@
+const store = {
+    price: '1USD',
+    item : "itemA",
+    price: "2USD",
+    item: "itemB"
+}
+
+//1USD = 80INR
+
+function exchangeCurrency(store){
+    let newObject = {}
+    
+}
+
+
